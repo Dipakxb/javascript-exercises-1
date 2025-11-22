52cfbe0ec38e77c380f0d639568b9f06f01f967b
Dipak <dipakxb@gmail.com>
2025-11-22 23:34:16 +0530

    getting started

--- 08_removeFromArray/removeFromArray.ts
+++ 08_removeFromArray/removeFromArray.ts
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array: Array<number> , ...args: Array<number>) {
+    return array.filter(val => !args.includes(val))
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
